09123653f4b48454fc366dce823e1c6799e6d306
kwon0144 <kinsey.w5505@gmail.com>
2025-02-06 23:29:29 +0800

    backend: api for dashboard data

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 /* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes"
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -19,6 +20,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 /* ROUTE */
+app.use("/dashboard", dashboardRoutes) // http://localhost:8000/dashboard
+
 app.get("/hello", (req, res) => {
     res.send("hello world");
 })
